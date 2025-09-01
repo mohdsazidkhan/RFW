c6b49be647b5605dac38ea62e41be14220b79cdb
tkwebdesigner <tofique.animation@gmail.com>
2025-09-01 13:12:40 +0530

    Build New

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 // Import Google Fonts
 const link = document.createElement('link');
 link.rel = 'stylesheet';
