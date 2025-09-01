c0f4c63834a358b4d8463ba95060f905dce1dd45
tkwebdesigner <tofique.animation@gmail.com>
2025-09-01 17:27:41 +0530

    Designers

--- src/pages/Designers.js
+++ src/pages/Designers.js
@@ -1,47 +1,218 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
 const Designers = () => {
+const designers = [
+{
+    "id": "1886",
+    "name": "1886",
+    "category": "STREETWEAR",
+    "image": "./images/designers/1886.webp"
+},
+{
+    "id": "abadia",
+    "name": "Abadia",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/abadia.webp"
+},
+{
+    "id": "adnan-akbar",
+    "name": "Adnan Akbar",
+    "category": "COUTURE",
+    "image": "./images/designers/adnan-akbar.webp"
+},
+{
+    "id": "aram",
+    "name": "Aram",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/aram.webp"
+},
+{
+    "id": "ashwaq-almarshad",
+    "name": "Ashwaq Al Marshad",
+    "category": "COUTURE",
+    "image": "./images/designers/ashwaq-almarshad.webp"
+},
+{
+    "id": "atelier-hikayat",
+    "name": "Atelier Hekayat",
+    "category": "COUTURE",
+    "image": "./images/designers/atelier-hikayat.webp"
+},
+{
+    "id": "awaken",
+    "name": "Awaken",
+    "category": "STREETWEAR",
+    "image": "./images/designers/atelier-hikayat.webp"
+},
+{
+    "id": "dar-alhanouf",
+    "name": "Dar alhanouf",
+    "category": "COUTURE",
+    "image": "./images/designers/dar-alhanouf.webp"
+},
+{
+    "id": "fatima-alabdul-qader",
+    "name": "Fatima AlAbdulQader",
+    "category": "COUTURE",
+    "image": "./images/designers/fatima-alabdul-qader.webp"
+},
+{
+    "id": "freeminds",
+    "name": "FREEMINDS",
+    "category": "STREETWEAR",
+    "image": "./images/designers/freeminds.webp"
+},
+{
+    "id": "hajruss",
+    "name": "Hajruss",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/hajruss.webp"
+},
+{
+    "id": "hala-algharbawi",
+    "name": "HALA ALGHARBAWI",
+    "category": "COUTURE",
+    "image": "./images/designers/hala-algharbawi.webp"
+},
+{
+    "id": "hindamme",
+    "name": "HINDAMME",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/hindamme.webp"
+},
+{
+    "id": "honayda",
+    "name": "Honayda",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/honayda.webp"
+},
+{
+    "id": "house-of-cenmar",
+    "name": "HOUSE OF CENMAR",
+    "category": "STREETWEAR",
+    "image": "./images/designers/house-of-cenmar.webp"
+},
+{
+    "id": "jubb",
+    "name": "Jubb",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/jubb.webp"
+},
+{
+    "id": "kaf-by-kaf",
+    "name": "Kaf by Kaf",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/kaf-by-kaf.webp"
+},
+{
+    "id": "khawla-alaiban",
+    "name": "KHAWLA ALAIBAN",
+    "category": "COUTURE",
+    "image": "./images/designers/khawla-alaiban.webp"
+},
+{
+    "id": "kml",
+    "name": "KML",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/kml.webp"
+},
+{
+    "id": "makram-marzuki",
+    "name": "MAKRAM MARZUKI",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/makram-marzuki.webp"
+},
+{
+    "id": "mashael-al-faris",
+    "name": "MASHAEL AL FARIS",
+    "category": "COUTURE",
+    "image": "./images/designers/mashael-al-faris.webp"
+},
+{
+    "id": "mazrood",
+    "name": "Mazrood",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/mazrood.webp"
+},
+{
+    "id": "noble-fresh",
+    "name": "Noble & Fresh",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/noble-fresh.webp"
+},
+{
+    "id": "nora-al-shaikh",
+    "name": "Nora Al Shaikh",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/nora-al-shaikh.webp"
+},
+{
+    "id": "nour-aldahri",
+    "name": "NOUR ALDHAHRI",
+    "category": "COUTURE",
+    "image": "./images/designers/nour-aldahri.webp"
+},
+{
+    "id": "noura-sulaiman",
+    "name": "Noura Sulaiman",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/noura-sulaiman.webp"
+},
+{
+    "id": "pavone",
+    "name": "PAVONE",
+    "category": "COUTURE",
+    "image": "./images/designers/pavone.webp"
+},
+{
+    "id": "qormuz",
+    "name": "QORMUZ",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/qormuz.webp"
+},
+{
+    "id": "razan-alazzouni",
+    "name": "RAZAN ALAZZOUNI",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/razan-alazzouni.webp"
+},
+{
+    "id": "rba",
+    "name": "RBA",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/rba.webp"
+},
+{
+    "id": "samar-nasraldin",
+    "name": "SAMAR NASRALDIN",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/samar-nasraldin.webp"
+},
+{
+    "id": "tima-abid",
+    "name": "Tima Abid",
+    "category": "COUTURE",
+    "image": "./images/designers/tima-abid.webp"
+},
+{
+    "id": "uscita",
+    "name": "USCITA",
+    "category": "READY-TO-WEAR",
+    "image": "./images/designers/uscita.webp"
+},
+{
+    "id": "waad-aloqaili",
+    "name": "Waad AloqAili",
+    "category": "COUTURE",
+    "image": "./images/designers/waad-aloqaili.webp"
+},
+{
+    "id": "yehya-al-bishri",
+    "name": "Yahya ALBISHRI",
+    "category": "COUTURE",
+    "image": "./images/designers/yehya-al-bishri.webp"
+},
+]
   const navigate = useNavigate();
-  
-  const designers = [
-    { name: "1880", category: "STREETWEAR", image: "designer-1880" },
-    { name: "ABADIA", category: "READY TO WEAR", image: "designer-abadia" },
-    { name: "ADNAN AKBAR", category: "COUTURE", image: "designer-adnan-akbar" },
-    { name: "DAR ALHANOUF", category: "READY TO WEAR", image: "designer-dar-alhanouf" },
-    { name: "FATIMA ALABDULQADER", category: "COUTURE", image: "designer-fatima-alabdulqader" },
-    { name: "HONAYDA", category: "READY TO WEAR", image: "designer-honayda" },
-    { name: "KML", category: "STREETWEAR", image: "designer-kml" },
-    { name: "PAVONE", category: "COUTURE", image: "designer-pavone" },
-    { name: "TIMA ABID", category: "READY TO WEAR", image: "designer-tima-abid" },
-    { name: "WAAD ALOQAILI", category: "COUTURE", image: "designer-waad-aloqaili" },
-    { name: "DESIGNER 11", category: "STREETWEAR", image: "designer-11" },
-    { name: "DESIGNER 12", category: "READY TO WEAR", image: "designer-12" },
-    { name: "DESIGNER 13", category: "COUTURE", image: "designer-13" },
-    { name: "DESIGNER 14", category: "READY TO WEAR", image: "designer-14" },
-    { name: "DESIGNER 15", category: "STREETWEAR", image: "designer-15" },
-    { name: "DESIGNER 16", category: "COUTURE", image: "designer-16" },
-    { name: "DESIGNER 17", category: "READY TO WEAR", image: "designer-17" },
-    { name: "DESIGNER 18", category: "STREETWEAR", image: "designer-18" },
-    { name: "DESIGNER 19", category: "COUTURE", image: "designer-19" },
-    { name: "DESIGNER 20", category: "READY TO WEAR", image: "designer-20" },
-    { name: "DESIGNER 21", category: "STREETWEAR", image: "designer-21" },
-    { name: "DESIGNER 22", category: "COUTURE", image: "designer-22" },
-    { name: "DESIGNER 23", category: "READY TO WEAR", image: "designer-23" },
-    { name: "DESIGNER 24", category: "STREETWEAR", image: "designer-24" },
-    { name: "DESIGNER 25", category: "COUTURE", image: "designer-25" },
-    { name: "DESIGNER 26", category: "READY TO WEAR", image: "designer-26" },
-    { name: "DESIGNER 27", category: "STREETWEAR", image: "designer-27" },
-    { name: "DESIGNER 28", category: "COUTURE", image: "designer-28" },
-    { name: "DESIGNER 29", category: "READY TO WEAR", image: "designer-29" },
-    { name: "DESIGNER 30", category: "STREETWEAR", image: "designer-30" },
-    { name: "DESIGNER 31", category: "COUTURE", image: "designer-31" },
-    { name: "DESIGNER 32", category: "READY TO WEAR", image: "designer-32" },
-    { name: "DESIGNER 33", category: "STREETWEAR", image: "designer-33" },
-    { name: "DESIGNER 34", category: "COUTURE", image: "designer-34" },
-    { name: "DESIGNER 35", category: "READY TO WEAR", image: "designer-35" }
-  ];
-
   return (
     <div className="designers-page">
       {/* Main Content Section */}
@@ -54,22 +225,27 @@ const Designers = () => {
                 <h1 className="designers-title">MEET THE RFW DESIGNERS</h1>
               </div>
               <div className="designers-description-section">
-                                 <p className="designers-description">
+                  <p className="designers-description">
                    Riyadh Fashion Week showcases exceptionally talented designers who contribute 
                    a unique perspective and a harmonious blend of innovation and tradition, 
                    promising to captivate and inspire and redefine the future of style.
-                 </p>
+                  </p>
               </div>
             </div>
             
             {/* Designers Grid */}
             <div className="designers-grid">
-              {designers.map((designer, index) => (
+              {designers?.map((designer, index) => (
                 <div key={index} className="designer-profile">
                   <div className="designer-image">
-                    <div className="designer-image-placeholder">
+                    <img 
+                      style={{width: '100%', height: '100%', objectFit: 'cover'}}
+                      src={designer.image} 
+                      alt={designer.name}
+                      />
+                    {/* <div className="designer-image-placeholder">
                       {designer.name.split(' ').map(word => word[0]).join('')}
-                    </div>
+                    </div> */}
                   </div>
                   <div className="designer-info">
                     <h3 className="designer-name">{designer.name}</h3>
