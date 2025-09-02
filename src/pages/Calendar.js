89769c174fb1862d6a75bdab16d43f6319e2694a
Hemant Arora <arorahemant75@gmail.com>
2025-09-02 11:57:30 +0530

    Calender Page Complete

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { designers } from '../utils/data/calendar';
-import { MdKeyboardArrowDown, MdKeyboardArrowUp, MdAdd } from 'react-icons/md';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp, MdAdd, MdRemove } from 'react-icons/md';
 import '../styles/Calendar.css';
 
 const Calendar = () => {
@@ -34,7 +34,7 @@ const Calendar = () => {
           <h1 className="calendar-title">RFW CALENDAR</h1>
           <p className="calendar-date-range">OCTOBER 17 - OCTOBER 21, 2025</p>
         </div>
-        
+
         {/* Day Accordions */}
         <div className="calendar-accordions">
           {designers.map((day, dayIndex) => (
@@ -45,33 +45,42 @@ const Calendar = () => {
                   {expandedDay === dayIndex ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />}
                 </div>
               </div>
-              
+
               {expandedDay === dayIndex && (
                 <div className="day-accordion-content">
                   <div className="designers-list">
-                    {day.designers.map((designer, designerIndex) => (
+                    {day.designers.map((designer) => (
                       <div key={designer.id} className="designer-item">
-                        <div className="designer-header" onClick={() => toggleDesigner(designer.id)}>
+                        <div
+                          className="designer-header"
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            cursor: 'pointer'
+                          }}
+                          onClick={() => toggleDesigner(designer.id)}
+                        >
                           <div className="designer-info">
                             <h3 className="designer-name">{designer.name}</h3>
                             <div className="designer-time">TIME: {designer.time}</div>
-                            <div className="designer-category">{designer.desc}</div>
                           </div>
-                          <div className="designer-action">
-                            {expandedDesigner === designer.id ? (
-                              <button 
-                                className="btn-learn-more"
-                                onClick={() => navigate(`/designer/${designer.id}`)}
-                              >
-                                LEARN MORE
-                              </button>
-                            ) : (
-                              <div className="expand-icon">
-                                <MdAdd />
-                              </div>
-                            )}
+                          <div className="expand-icon" style={{ marginLeft: '16px', fontSize: '2rem' }}>
+                            {expandedDesigner === designer.id ? <MdRemove /> : <MdAdd />}
                           </div>
                         </div>
+
+                        {expandedDesigner === designer.id && (
+                          <div className="designer-action">
+                            <div className="designer-category">{designer.desc}</div>
+                            <button
+                              className="btn-learn-more"
+                              onClick={() => navigate(`/designer/${designer.id}`)}
+                            >
+                              LEARN MORE
+                            </button>
+                          </div>
+                        )}
                       </div>
                     ))}
                   </div>
@@ -86,4 +95,3 @@ const Calendar = () => {
 };
 
 export default Calendar;
-
