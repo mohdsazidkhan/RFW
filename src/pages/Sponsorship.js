cce5c8c6f7a0a2ae97428ca339713aab9d0794a2
mohdsazidkhan <sajidpahat786@gmail.com>
2025-10-07 01:38:21 +0530

    New Calender TIMA

--- src/pages/Sponsorship.js
+++ src/pages/Sponsorship.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import '../styles/Sponsorship.css'
 const Sponsorship = () => {
-const previousPartners = [
-  { name: "anb arab national bank", logo: require('../images/partners/arabnationalbank.webp'), url:"https://anb.com.sa/web/anb" },
-  { name: "cenomi", logo: require('../images/partners/Cenomi.webp'), url:"https://centers.cenomi.com/sa-en/" },
-  { name: "Charlotte Tilbury", logo: require('../images/partners/Charlotte.webp'), url:"https://www.sa.charlottetilbury.com/en/" },
-  { name: "Genesis", logo: require('../images/partners/genesis.webp'), url:"https://www.genesis.com/sa-riyadh/ar/main.html" },
-  { name: "AK AL KHOZAMA INVESTMENT", logo: require('../images/partners/ALKHOZAMA.webp'), url:"https://alkhozama.com/en/" },
-  { name: "KAFD", logo: require('../images/partners/KAFD.webp'), url:"https://www.kafd.sa/en/" },
-  { name: "ASTERI", logo: require('../images/partners/Asteri.webp'), url:"https://www.asteribeauty.com/" },
-  { name: '', logo: '', url:"" },
-  { name: "MANDARIN ORIENTAL AL FAISALIAH RIYADH", logo: require('../images/partners/MORYD.webp'), url:"https://www.mandarinoriental.com/en/riyadh/olaya" },
-  { name: "S MEDIA", logo: require('../images/partners/SSMedia.webp'), url:"https://www.saudisigns.com/" },
-  { name: "BEAST Radio", logo: require('../images/partners/MDLBEAST.webp'), url:"https://mdlbeast.com/" },
-  { name: "Schweppes", logo: require('../images/partners/Schweppes.webp'), url:"https://www.schweppes.eu/" }
+const officialPartners = [
+  { name: "anb arab national bank", logo: require('../images/partners/saudia.png'), url:"https://www.saudia.com/?utm_campaign=search&utm_source=cpc&utm_medium=google&gclsrc=aw.ds&cid=&gad_source=1&gad_campaignid=23017945566&gbraid=0AAAAADwJ54eKETFOpCCnGQ0o8dggT-PWk&gclid=EAIaIQobChMIxO7g9KaQkAMVCAcGAB3Kty9SEAAYASAAEgL--vD_BwE" },
+  { name: "cenomi", logo: require('../images/partners/cenomi.png'), url:"https://centers.cenomi.com/sa-en/" },
+];
+const strategicPartners = [
+  { name: "Bank Saudi Fransi", logo: require('../images/partners/bsf.png'), url:"https://bsf.sa/english/home" },
+  { name: "Genesis", logo: require('../images/partners/genesis.png'), url:"https://www.genesis.com/us/en/home" }
+];
+const supportingPartners1 = [
+  { name: "Eyewa", logo: require('../images/partners/eyewa.png'), url:"https://eyewa.com/sa-ar" },
+  { name: "Rotana Signs", logo: require('../images/partners/rotana-signs.png'), url:"https://www.linkedin.com/company/rotanasigns/" },
+  { name: "Ounass", logo: require('../images/partners/ounass.png'), url:"https://en-saudi.ounass.com/women?gclsrc=aw.ds&gad_source=1&gad_campaignid=22595114047&gbraid=0AAAAADSMPX6nAHHbpMqeNbaFxZ-AV7TzP&gclid=EAIaIQobChMIjJWVz6yQkAMVHwwGAB0elitaEAAYASAAEgJKAPD_BwE" },
+];
+const supportingPartners2 = [
+  { name: "Mandarin Oriental", logo: require('../images/partners/mandarin.png'), url:"https://www.mandarinoriental.com/en" },
+  { name: "AK AL KHOZAMA INVESTMENT", logo: require('../images/partners/al-khozama.png'), url:"https://alkhozama.com/en/" },
 ];
 const isMobile = window.innerWidth < 768;
   return (
@@ -60,40 +63,128 @@ const isMobile = window.innerWidth < 768;
 
       {/* Previous Partners Section */}
    <section className="previous-partners-section">
-  <div className="sponsorship-section-container">
+    <div className="sponsorship-section-container">
+      <h2 className="partners-title">OUR PARTNERS</h2>
     <div className="previous-partners-content">
-      <h2 className="previous-partners-title">PREVIOUS PARTNERS</h2>
-      <div className="partners-grid">
-  {previousPartners.map((partner, index) => {
-    const isBlank = !partner.name && !partner.logo;
+      <h4 className="previous-partners-title one">OFFICIAL PARTNERS</h4>
+      <div className="partners-logos one">
+      {officialPartners.map((partner, index) => {
 
-    return (
-      <div
-        key={index}
-        style={{display: isBlank && isMobile && "none", minHeight: isBlank && 0,minWidth: isBlank && 0, height: isBlank && 0, boxShadow: isBlank && 'none', padding: isBlank && 0  }}
-        className={`partner-logo`} // hide on mobile if blank
-      >
-        <a
-          className="partner-logo-image"
-          href={partner.url}
-          target="_blank"
-          style={{display: isBlank && isMobile && "none", minHeight: isBlank && 0, minWidth: isBlank && 0, height: isBlank && 0  }}
-          rel="noopener noreferrer"
+        return (
+        <div
+          key={index}
+          style={{display: isMobile && "none" }}
+          className={`partner-logo`} // hide on mobile if blank
         >
-          {partner.logo && (
-            <img
-              src={partner.logo}
-              alt={partner.name || 'Partner'}
-              className="partner-img"
-            />
-          )}
-        </a>
+          <a
+            className="partner-logo-image"
+            href={partner.url}
+            target="_blank"
+            style={{display: isMobile && "none"  }}
+            rel="noopener noreferrer"
+          >
+            {partner.logo && (
+              <img
+                src={partner.logo}
+                alt={partner.name || 'Partner'}
+                className="partner-img"
+              />
+            )}
+          </a>
+        </div>
+        );
+      })}
       </div>
-    );
-  })}
-</div>
+      </div>
+      <div className="previous-partners-content">
+      <h4 className="previous-partners-title two">STRATEGIC PARTNERS</h4>
+      <div className="partners-logos two">
+      {strategicPartners.map((partner, index) => {
+        return (
+        <div
+          key={index}
+          style={{display: isMobile && "none" }}
+          className={`partner-logo`} // hide on mobile if blank
+        >
+          <a
+            className="partner-logo-image"
+            href={partner.url}
+            target="_blank"
+            style={{display: isMobile && "none"  }}
+            rel="noopener noreferrer"
+          >
+            {partner.logo && (
+              <img
+                src={partner.logo}
+                alt={partner.name || 'Partner'}
+                className="partner-img"
+              />
+            )}
+          </a>
+        </div>
+        );
+        })}
+      </div>
 
-    </div>
+      </div>
+      <div className="previous-partners-content">
+      <h4 className="previous-partners-title three">SUPPORTING PARTNERS</h4>
+      <div className="partners-logos three">
+      {supportingPartners1.map((partner, index) => {
+        return (
+        <div
+          key={index}
+          style={{display: isMobile && "none" }}
+          className={`partner-logo`} // hide on mobile if blank
+        >
+          <a
+            className="partner-logo-image"
+            href={partner.url}
+            target="_blank"
+            style={{display: isMobile && "none"  }}
+            rel="noopener noreferrer"
+          >
+            {partner.logo && (
+              <img
+                src={partner.logo}
+                alt={partner.name || 'Partner'}
+                className="partner-img"
+              />
+            )}
+          </a>
+        </div>
+        );
+        })}
+      </div>
+      <div className="partners-logos four">
+      {supportingPartners2.map((partner, index) => {
+        return (
+        <div
+          key={index}
+          style={{display: isMobile && "none" }}
+          className={`partner-logo`} // hide on mobile if blank
+        >
+          <a
+            className="partner-logo-image"
+            href={partner.url}
+            target="_blank"
+            style={{display: isMobile && "none"  }}
+            rel="noopener noreferrer"
+          >
+            {partner.logo && (
+              <img
+                src={partner.logo}
+                alt={partner.name || 'Partner'}
+                className="partner-img"
+              />
+            )}
+          </a>
+        </div>
+        );
+        })}
+      </div>
+
+      </div>
   </div>
 </section>
 
